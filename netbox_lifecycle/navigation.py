21f6f9aa6f15cd283dec1ce212498f5c5d87d733
Daniel Sheppard <dans@dansheps.com>
2023-08-24 15:13:30 -0500

    Add permissions around menu items.

--- netbox_lifecycle/navigation.py
+++ netbox_lifecycle/navigation.py
@@ -4,31 +4,38 @@ from utilities.choices import ButtonColorChoices
 lifecycle = PluginMenuItem(
     link='plugins:netbox_lifecycle:hardwarelifecycle_list',
     link_text='Hardware Lifecycle',
+    permissions=['netbox_lifecycle.view_hardwarelifecycle'],
 )
 
 vendors = PluginMenuItem(
     link='plugins:netbox_lifecycle:vendor_list',
     link_text='Vendors',
+    permissions=['netbox_lifecycle.view_vendor'],
 )
 skus = PluginMenuItem(
     link='plugins:netbox_lifecycle:supportsku_list',
     link_text='Support SKUs',
+    permissions=['netbox_lifecycle.view_supportsku'],
 )
 contracts = PluginMenuItem(
     link='plugins:netbox_lifecycle:supportcontract_list',
     link_text='Contracts',
+    permissions=['netbox_lifecycle.view_supportcontrnact'],
 )
 contract_assignments = PluginMenuItem(
     link='plugins:netbox_lifecycle:supportcontractassignment_list',
     link_text='Contract Assignments',
+    permissions=['netbox_lifecycle.view_supportcontractassignment'],
 )
 licenses = PluginMenuItem(
     link='plugins:netbox_lifecycle:license_list',
     link_text='Licenses',
+    permissions=['netbox_lifecycle.view_license'],
 )
 license_assignments = PluginMenuItem(
     link='plugins:netbox_lifecycle:licenseassignment_list',
     link_text='License Assignments',
+    permissions=['netbox_lifecycle.view_licenseassignment'],
 )
 
 
