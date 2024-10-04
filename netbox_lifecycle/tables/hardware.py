8f74347c4efb140b2fda85e0c7a56c99a1fe36a0
Daniel Sheppard <dans@dansheps.com>
2024-10-04 16:49:55 -0500

    Fixes: #47 - Fix sorting bug

--- netbox_lifecycle/tables/hardware.py
+++ netbox_lifecycle/tables/hardware.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext as _
 import django_tables2 as tables
 
 from netbox.tables import NetBoxTable
@@ -12,15 +13,17 @@ __all__ = (
 class HardwareLifecycleTable(NetBoxTable):
     name = tables.Column(
         linkify=True,
-        accessor='name'
+        accessor='name',
+        orderable=False,
     )
-
     assigned_object = tables.Column(
         linkify=True,
-        verbose_name='Hardware'
+        verbose_name=_('Hardware'),
+        orderable=False,
     )
     assigned_object_count = tables.Column(
-        verbose_name='Assigned Object Count'
+        verbose_name=_('Assigned Object Count'),
+        orderable=False,
     )
 
     class Meta(NetBoxTable.Meta):
