7b6b1183a3cdc8b23afe6844477ec7bc25fcc464
uck9 <nathan.a.reeves@gmail.com>
2024-10-13 09:30:05 +0800

    Update sync_cisco_hw_eox_data.py
    Add data sync options to improve import of EoX data

--- netbox_lifecycle/management/commands/sync_cisco_hw_eox_data.py
+++ netbox_lifecycle/management/commands/sync_cisco_hw_eox_data.py
@@ -8,12 +8,16 @@ from django.core.exceptions import MultipleObjectsReturned
 from django.contrib.contenttypes.models import ContentType
 from datetime import datetime
 
-from dcim.models import DeviceType, ModuleType, Manufacturer
+from dcim.models import Device, DeviceType, Module, ModuleType, Manufacturer
 from netbox_lifecycle.models import hardware
 
 
 class Command(BaseCommand):
     help = 'Sync Hardware Lifecycle Information from Cisco EoX Support API'
+
+    TRACK_ONLY_ACTIVE_PIDS = bool
+    API_IS_SOURCE_OF_TRUTH = bool
+    SET_MISSING_DATA_AS_END_OF_SUPPORT = bool
 
     def add_arguments(self, parser):
         # Named (optional) arguments
@@ -28,6 +32,9 @@ class Command(BaseCommand):
         PLUGIN_SETTINGS = settings.PLUGINS_CONFIG.get("netbox_lifecycle", dict())
         CISCO_CLIENT_ID = PLUGIN_SETTINGS.get("cisco_support_api_client_id", "")
         CISCO_CLIENT_SECRET = PLUGIN_SETTINGS.get("cisco_support_api_client_secret", "")
+        self.TRACK_ONLY_ACTIVE_PIDS = PLUGIN_SETTINGS.get("track_only_active_pids", "True")
+        self.API_IS_SOURCE_OF_TRUTH = PLUGIN_SETTINGS.get("api_is_source_of_truth", "True")
+        self.SET_MISSING_DATA_AS_END_OF_SUPPORT = PLUGIN_SETTINGS.get("set_missing_data_as_end_of_support", "True")
 
         token_url = "https://id.cisco.com/oauth2/default/v1/token"
         data = {'grant_type': 'client_credentials', 'client_id': CISCO_CLIENT_ID, 'client_secret': CISCO_CLIENT_SECRET}
@@ -42,42 +49,57 @@ class Command(BaseCommand):
 
     def update_lifecycle_data(self, pid, hardware_type, eox_data):
 
-        hw_obj = DeviceType()
+        self.stdout.write(self.style.SUCCESS(f"{pid} - {hardware_type}"))
+
         content_type = ContentType()
 
         match hardware_type:
             case "devicetype":
+                hw_obj = DeviceType()
                 content_type = ContentType.objects.get(app_label="dcim", model="devicetype")
                 try:
                     # Get the device type object for the supplied PID
                     hw_obj = DeviceType.objects.get(part_number=pid)
+                    hw_count = Device.objects.filter(device_type=hw_obj).count()
+                    self.stdout.write(self.style.SUCCESS(f"{pid} - {hw_count} active devices"))
                 except MultipleObjectsReturned:
                     # Error if Netbox returns multiple duplicate PN's
                     self.stdout.write(self.style.NOTICE(f"ERROR: Multiple objects exist with Part Number {pid}"))
                     return
 
             case "moduletype":
+                hw_obj = ModuleType()
                 content_type = ContentType.objects.get(app_label="dcim", model="moduletype")
                 try:
                     # Get the device type object for the supplied PID
                     hw_obj = ModuleType.objects.get(part_number=pid)
+                    hw_count = Module.objects.filter(module_type=hw_obj).count()
+                    self.stdout.write(self.style.SUCCESS(f"{pid} - {hw_count} active moduletypes"))
                 except MultipleObjectsReturned:
                     # Error if Netbox returns multiple duplicate PN's
                     self.stdout.write(self.style.NOTICE(f"ERROR: Multiple objects exist with Part Number {pid}"))
                     return
 
             case _:
-                raise CommandError('Invalid hardware_type argument defined.')
+                raise CommandError(f'Invalid hardware_type argument defined.')
                 exit
 
         # Check if a HardwareLifecycle record already exists
         try:
-            hw_lifecycle = hardware.HardwareLifecycle.objects.get(assigned_object_id=hw_obj.id, assigned_object_type_id=content_type.id)
+            hw_lifecycle = hardware.HardwareLifecycle.objects.get(assigned_object_id=hw_obj.id)
             self.stdout.write(self.style.SUCCESS(f"{pid} - has an existing NetBox hardware lifecycle record"))
+            if ((hw_count == 0) and (self.TRACK_ONLY_ACTIVE_PIDS)):
+                self.stdout.write(self.style.NOTICE(f"{pid} - has no active hardware with this PID - We're tracking only active PIDs - Deleting Lifecycle record"))
+                hw_lifecycle.delete()
+                return
         # If not, create a new one for this Device Type
         except hardware.HardwareLifecycle.DoesNotExist:
-            hw_lifecycle = hardware.HardwareLifecycle(assigned_object_id=hw_obj.id, assigned_object_type_id=content_type.id)
-            self.stdout.write(self.style.NOTICE(f"{pid} - has no existing NetBox hardware lifecycle record"))
+            if ((hw_count == 0) and (self.TRACK_ONLY_ACTIVE_PIDS)):
+                self.stdout.write(self.style.NOTICE(f"{pid} - no active hardware with this PID - We're only tracking active PIDs - no Lifecycle record created"))
+                return
+            else:
+                hw_lifecycle = hardware.HardwareLifecycle(assigned_object_id=hw_obj.id, assigned_object_type_id=content_type.id)
+                self.stdout.write(self.style.NOTICE(f"{pid} - has no existing NetBox hardware lifecycle record"))
 
         # Only save if something has changed
         value_changed = False
@@ -133,18 +155,18 @@ class Command(BaseCommand):
             self.stdout.write(self.style.NOTICE(f"{pid} - has no end_of_security_vul_support_date"))
 
         try:
-            if not eox_data["EOXRecord"][0]["EndOfSvcAttachDate"]["value"]:
-                self.stdout.write(self.style.NOTICE(f"{pid} - has no end_of_service_attach_date"))
+            if not eox_data["EOXRecord"][0]["EndOfServiceContractRenewal"]["value"]:
+                self.stdout.write(self.style.NOTICE(f"{pid} - has no end_of_service_contract_renewal"))
             else:
-                last_contract_date_string = eox_data["EOXRecord"][0]["EndOfSvcAttachDate"]["value"]
+                last_contract_date_string = eox_data["EOXRecord"][0]["EndOfServiceContractRenewal"]["value"]
                 last_contract_date_date = datetime.strptime(last_contract_date_string, '%Y-%m-%d').date()
-                self.stdout.write(self.style.SUCCESS(f"{pid} - end_of_service_attach_date: {last_contract_date_date}"))
+                self.stdout.write(self.style.SUCCESS(f"{pid} - end_of_service_contract_renewal: {last_contract_date_date}"))
 
                 if hw_lifecycle.last_contract_date != last_contract_date_date:
                     hw_lifecycle.last_contract_date = last_contract_date_date
                     value_changed = True
         except KeyError:
-            self.stdout.write(self.style.NOTICE(f"{pid} - has no end_of_service_attach_date"))
+            self.stdout.write(self.style.NOTICE(f"{pid} - has no end_of_service_contract_renewal"))
 
         try:
             if not eox_data["EOXRecord"][0]["LastDateOfSupport"]["value"]:
@@ -162,6 +184,13 @@ class Command(BaseCommand):
             self.stdout.write(self.style.NOTICE(f"{pid} - has no last_date_of_support"))
 
         if (value_changed and end_of_sale_defined and end_of_support_defined):
+            if (self.SET_MISSING_DATA_AS_END_OF_SUPPORT):
+                # Check whether end of security is blank.  Use end_of_support value in that case
+                if (hw_lifecycle.end_of_security is None):
+                    hw_lifecycle.end_of_security = hw_lifecycle.end_of_support
+                if (hw_lifecycle.end_of_maintenance is None):
+                    hw_lifecycle.end_of_maintenance = hw_lifecycle.end_of_support
+            # Save the record
             hw_lifecycle.save()
 
         return
@@ -201,7 +230,7 @@ class Command(BaseCommand):
 
                 self.stdout.write(self.style.SUCCESS(f'Found device type "{moduletype}" with Part Number "{moduletype.part_number}"'))
                 results[moduletype.part_number] = 'moduletype'
-        except DeviceType.DoesNotExist:
+        except ModuleType.DoesNotExist:
             raise CommandError(f'Manufacturer "{manufacturer_results}" has no Module Types')
 
         return results
@@ -220,10 +249,10 @@ class Command(BaseCommand):
         for pid, hw_type in product_ids.items():
             url = f'https://apix.cisco.com/supporttools/eox/rest/5/EOXByProductID/1/{pid}?responseencoding=json'
             api_call_response = requests.get(url, headers=api_call_headers)
-            self.stdout.write(self.style.SUCCESS('Call ' + url))
-
+            self.stdout.write(self.style.SUCCESS('#######################################################'))
+            self.stdout.write(self.style.SUCCESS(f"Calling {url}"))
             # sanatize file name
-            filename = django.utils.text.get_valid_filename(f'{pid}.json')
+            filename = django.utils.text.get_valid_filename(f"{pid}.json")
 
             # debug API answer to text file
             # with open('/opt/netbox_Lifecycle_cisco_xapi_results/%s' % filename, 'w') as outfile:
