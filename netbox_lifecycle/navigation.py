c4b58f77f471fac706a1de1d6f9ee734ce61ddee
Nate Cohen <94263748+natecohen@users.noreply.github.com>
2025-01-29 10:28:34 -0500

    Simplify menu generation and implement add buttons

--- netbox_lifecycle/navigation.py
+++ netbox_lifecycle/navigation.py
@@ -1,49 +1,57 @@
-from netbox.plugins import PluginMenuItem, PluginMenu
+from netbox.plugins import PluginMenu, PluginMenuButton, PluginMenuItem
 
-lifecycle = PluginMenuItem(
-    link='plugins:netbox_lifecycle:hardwarelifecycle_list',
-    link_text='Hardware Lifecycle',
-    permissions=['netbox_lifecycle.view_hardwarelifecycle'],
-)
+APP_LABEL = 'netbox_lifecycle'
 
-vendors = PluginMenuItem(
-    link='plugins:netbox_lifecycle:vendor_list',
-    link_text='Vendors',
-    permissions=['netbox_lifecycle.view_vendor'],
-)
-skus = PluginMenuItem(
-    link='plugins:netbox_lifecycle:supportsku_list',
-    link_text='Support SKUs',
-    permissions=['netbox_lifecycle.view_supportsku'],
-)
-contracts = PluginMenuItem(
-    link='plugins:netbox_lifecycle:supportcontract_list',
-    link_text='Contracts',
-    permissions=['netbox_lifecycle.view_supportcontract'],
-)
-contract_assignments = PluginMenuItem(
-    link='plugins:netbox_lifecycle:supportcontractassignment_list',
-    link_text='Contract Assignments',
-    permissions=['netbox_lifecycle.view_supportcontractassignment'],
-)
-licenses = PluginMenuItem(
-    link='plugins:netbox_lifecycle:license_list',
-    link_text='Licenses',
-    permissions=['netbox_lifecycle.view_license'],
-)
-license_assignments = PluginMenuItem(
-    link='plugins:netbox_lifecycle:licenseassignment_list',
-    link_text='License Assignments',
-    permissions=['netbox_lifecycle.view_licenseassignment'],
-)
 
+def get_model_buttons(model_name, actions=('add',)):
+    buttons = []
+
+    if 'add' in actions:
+        buttons.append(
+            PluginMenuButton(
+                link=f'plugins:{APP_LABEL}:{model_name}_add',
+                title='Add',
+                icon_class='mdi mdi-plus-thick',
+                permissions=[f'{APP_LABEL}.add_{model_name}'],
+            )
+        )
+
+    if 'import' in actions:
+        buttons.append(
+            PluginMenuButton(
+                link=f'plugins:{APP_LABEL}:{model_name}_import',
+                title='Import',
+                icon_class='mdi mdi-upload',
+                permissions=[f'{APP_LABEL}.add_{model_name}'],
+            )
+        )
+
+    return buttons
+
+
+def get_model_item(model_name, label, actions=('add',)):
+    return PluginMenuItem(
+        link=f'plugins:{APP_LABEL}:{model_name}_list',
+        link_text=label,
+        permissions=[f'{APP_LABEL}.view_{model_name}'],
+        buttons=get_model_buttons(model_name, actions),
+    )
+
+
+hardwarelifecycle_item = get_model_item('hardwarelifecycle', 'Hardware Lifecycle')
+vendor_item = get_model_item('vendor', 'Vendors')
+supportsku_item = get_model_item('supportsku', 'Support SKUs')
+supportcontract_item = get_model_item('supportcontract', 'Contracts')
+supportcontractassignment_item = get_model_item('supportcontractassignment', 'Contract Assignments')
+license_item = get_model_item('license', 'Licenses')
+licenseassignment_item = get_model_item('licenseassignment', 'License Assignments')
 
 menu = PluginMenu(
     label='Hardware Lifecycle',
     groups=(
-        ('Lifecycle', (lifecycle, )),
-        ('Support Contracts', (vendors, skus, contracts, contract_assignments)),
-        ('Licensing', (licenses, license_assignments)),
+        ('Lifecycle', (hardwarelifecycle_item,)),
+        ('Support Contracts', (vendor_item, supportsku_item, supportcontract_item, supportcontractassignment_item)),
+        ('Licensing', (license_item, licenseassignment_item)),
     ),
-    icon_class='mdi mdi-server'
+    icon_class='mdi mdi-server',
 )
